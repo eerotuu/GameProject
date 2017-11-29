7b1f9e472158386cd767c0819e5e8f4b554d48a3
hoki9 <henrihok@metropolia.fi>
2017-11-29 10:26:57 +0200

    added game music

---
+++ Assets/Music.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+	public static Music current;
+
+	void Awake() {
+		if (current != null) Destroy (gameObject);
+		current = this;
+
+		DontDestroyOnLoad(transform.gameObject);
+	}
+}
---
+++ Assets/MusicChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicChange : MonoBehaviour {
+	public AudioClip audio;
+
+	// Use this for initialization
+	void Start () {
+		Music.current.gameObject.GetComponent<AudioSource> ().clip = audio;
+		Music.current.gameObject.GetComponent<AudioSource> ().Play ();
+	}
+	
+
+}
