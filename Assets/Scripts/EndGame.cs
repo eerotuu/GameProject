8f16205b4a73e5eaaadd947b917e5d918563a1e5
hoki9 <henrihok@metropolia.fi>
2017-12-05 10:19:12 +0200

    added end scene

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour {
+
+
+	private PointerController endgame;
+
+	void Start()
+	{
+
+		endgame = GameObject.Find ("Start").GetComponent<PointerController> ();
+
+	}
+
+	void Update(){
+
+		if (endgame.getPressed()) {
+
+			SceneManager.LoadScene ("main_menu");
+		}
+
+	}
+}
